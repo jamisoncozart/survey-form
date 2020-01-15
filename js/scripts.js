5a12555216ab281b2feef99daaa7b2eec4c08a75
Abraham Ybarra and Jamison Cozart <jamison@>
2020-01-15 13:55:50 -0800

    add conditional javascript to display different colored balls, and add styling

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,18 @@ $(document).ready(function(){
   $("#form").submit(function(){
     event.preventDefault();
     var name = $("#name").val();
-    console.log(name)
+    var address = $("#address").val();
+    var color = $("#color").val();
+    $("#nameOutput").text(name);
+    $("#addressOutput").text(address);
+    
+    //Color selectors for ball
+    if(color === "Red") {
+      $("#ball").attr("src", "img/redball.jpg");
+    } else if(color === "Blue") {
+      $("#ball").attr("src", "img/blueball.jpg");
+    } else if(color === "Yellow") {
+      $("#ball").attr("src", "img/yellowball.jpg");
+    }
   });
 });
